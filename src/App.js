99692e507a33c6952ab42124f94cce9c7ca697d9
srinidhinitya <96458682+srinidhinitya@users.noreply.github.com>
2025-01-10 19:16:20 +0530

    inital

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Card from "./components/Card";
+import Navbar from "./components/Navbar";
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Card />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Card.jsx
@@ -0,0 +1,3 @@
+export default function Card() {
+  return <h1 className="text-3xl font-bold text-blue-300">Hello, welcome!</h1>;
+}
