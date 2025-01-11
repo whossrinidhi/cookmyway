5d1e05c0a249493a5cc2b69c6d488ca3657de6cf
srinidhinitya <96458682+srinidhinitya@users.noreply.github.com>
2025-01-11 21:42:44 +0530

    navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,26 @@
 import { NavLink } from "react-router-dom";
 export default function Navbar() {
   return (
-    <div>
-      <ul className="navbar">
-        <li>
-          <NavLink to="/">Home</NavLink>
-        </li>
-        <li>
-          <NavLink to="/saved">Saved</NavLink>
-        </li>
-        <li>
-          <NavLink to="/plan">Plan</NavLink>
-        </li>
-      </ul>
+    <div className="flex flex-row bg-gray-400 h-14  justify-center">
+      <div className="flex space-x-4 space-y-4 float-left">
+        <ul className="flex space-x-16">
+          <li>
+            <NavLink to="/">Home</NavLink>
+          </li>
+          <li>
+            <NavLink to="/saved">Saved</NavLink>
+          </li>
+          <li>
+            <NavLink to="/plan">Plan</NavLink>
+          </li>
+        </ul>
+        <button className="bg-teal-600 hover:bg-teal-200 hover:text-black text-white font-bold py-2 px-4 rounded">
+          Login
+        </button>
+        <button className="bg-teal-200 hover:bg-teal-600 hover:text-white text-black font-bold py-2 px-4 rounded">
+          Signup
+        </button>
+      </div>
     </div>
   );
 }
